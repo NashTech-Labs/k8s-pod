d90a8a54abfcc527b4dfbe463ff5be8f0ef47a56
yamikarajput546 <yamikarajput.233@gmail.com>
2022-06-24 17:35:43 +0530

    files added

---
+++ README.md
@@ -0,0 +1,64 @@
+#### Python client for the kubernetes API for pods
+
+we can use the client module to interact with the resources. 
+
+`Resources:` kubectl get commands are used to create pod using yaml files in a cluster for eg:
+
+To create the pod in the cluster, we fire following kubectl command:
+
+```kubectl apply -f pod.yaml``` 
+
+Get the list of all pod
+
+`kubectl get pods`
+
+But In Python, we instantiate CoreV1Api class from client module:
+
+`client_api = client.CoreV1Api()`         
+
+Here I've created the client with it's respective class CoreV1Api
+and storing in a var named as client_api. so furture we can use it.
+
+`KubeConfig:` to pass the on local cluster e.g minikube we use bellowcommand: 
+
+`config. load_kube_config()`
+
+#### Authenticating to the Kubernetes API server
+
+But what if you want to list all the automated ingress of a GKE Cluster, you must need to authenticate the configuration
+
+`configuration.api_key = {"authorization": "Bearer" + bearer_token}` 
+
+I've used Bearer Token which enable requests to authenticate using an access key.
+
+### create pods:
+
+call the function create_pod(cluster_details)
+
+And run following command:
+
+`python3 pod.py`
+
+#### get the list of all pods:
+
+call the function get_pods(cluster_details)
+
+And run following command:
+
+`python3 pod.py`
+
+#### update pods:
+
+Call the function update_pod(cluster_details,k8s_object_name="nginx-pod-yamika")
+
+NOTE: You can't update the metadata name of existing pod.
+
+And run following command:
+
+`python3 pod.py`
+
+#### delete pods:
+
+call the function delete_pod(cluster_details,k8s_object_name="nginx-pod-yamika")
+
+`python3 pod.py`
---
+++ pod.py
@@ -0,0 +1,146 @@
+from kubernetes import client
+from kubernetes.client import ApiClient
+import json
+from kubernetes.client.rest import ApiException
+import json,base64,yaml
+
+def __get_kubernetes_corev1client(bearer_token,api_server_endpoint):
+    try:
+        #corev1api=("pod","node","config_map","secret","service","namespace","resource")
+        configuration = client.Configuration()
+        configuration.host = api_server_endpoint
+        configuration.verify_ssl = False
+        configuration.api_key = {"authorization": "Bearer " + bearer_token}
+        client.Configuration.set_default(configuration)
+        client_api = client.CoreV1Api()
+        return client_api
+    except Exception as e:
+        print("Error getting kubernetes client \n{}".format(e))
+        return None
+
+def __format_data_for_pod(client_output):
+        temp_dict={}
+        temp_list=[]
+        json_data=ApiClient().sanitize_for_serialization(client_output)
+        if len(json_data["items"]) != 0:
+            for pod in json_data["items"]:
+                temp_dict={
+                    "pod": pod["metadata"]["name"],
+                    "namespace": pod["metadata"]["namespace"],
+                    "status": pod["status"]["phase"]
+                }
+                temp_list.append(temp_dict)
+        return temp_list
+
+def __format_data_for_create_pod(client_output):
+        temp_dict={}
+        temp_list=[]
+        json_data=ApiClient().sanitize_for_serialization(client_output)
+        #print("JSON_DATA OF KUBERNETES OBJECT:{}".format(json_data))
+        
+        if type(json_data) is str:
+            print("FORMAT_DATA :{}".format(type(json_data)))
+            json_data = json.loads(json_data)
+        temp_list.append(json_data)
+        return temp_list
+    
+
+def get_pods(cluster_details,namespace="default",all_namespaces=False):
+        client_api= __get_kubernetes_corev1client(
+            bearer_token=cluster_details["bearer_token"],
+            api_server_endpoint=cluster_details["api_server_endpoint"],
+        )
+        if all_namespaces is True:
+            pod_list =client_api.list_pod_for_all_namespaces(watch=False)
+            data=__format_data_for_pod(pod_list)
+            print("Pods under all namespaces: {}".format(data))
+            # return data
+        else:
+            pod_list = client_api.list_namespaced_pod(namespace)
+            data=__format_data_for_pod(pod_list)
+            print("Pods under namespaces {}: {}".format(namespace,data))
+
+def create_pod(cluster_details,yaml_body=None,namespace="default"):
+    # Configs can be set in Configuration class directly or using helper
+    # utility. If no argument provided, the config will be loaded from
+    # default location.
+    try:
+        client_api= __get_kubernetes_corev1client(
+                bearer_token=cluster_details["bearer_token"],
+                api_server_endpoint=cluster_details["api_server_endpoint"],
+            )
+
+        yaml_data=open("pod-create.yml", "rb").read().decode('utf-8')
+        yaml_body=yaml.safe_load(yaml_data)
+
+        resp = client_api.create_namespaced_pod(
+            body=yaml_body, namespace="{}".format(namespace))
+
+        data=__format_data_for_create_pod(resp)
+        print("Pod created: {}".format(data))
+
+    except ApiException as e:
+        print("ERROR IN create_deployment:\n{}".format(e.body))
+        print("TYPE :{}".format(type(e)))
+        return __format_data_for_create_pod(e.body)
+
+def update_pod(cluster_details,k8s_object_name=None,yaml_body=None,namespace="default"):
+    # Configs can be set in Configuration class directly or using helper
+    # utility. If no argument provided, the config will be loaded from
+    # default location.
+    try:
+        client_api= __get_kubernetes_corev1client(
+                bearer_token=cluster_details["bearer_token"],
+                api_server_endpoint=cluster_details["api_server_endpoint"],
+            )
+
+        yaml_data=open("pod-update.yml", "rb").read().decode('utf-8')
+        yaml_body=yaml.safe_load(yaml_data)
+
+        resp = client_api.patch_namespaced_pod(
+            name=k8s_object_name,
+            body=yaml_body, 
+            namespace="{}".format(namespace))
+
+        data=__format_data_for_create_pod(resp)
+        print("Pod updated: {}".format(data))
+
+    except ApiException as e:
+        print("ERROR IN create_deployment:\n{}".format(e.body))
+        print("TYPE :{}".format(type(e)))
+        return __format_data_for_create_pod(e.body)
+
+def delete_pod(cluster_details,k8s_object_name=None,namespace="default"):
+    # Configs can be set in Configuration class directly or using helper
+    # utility. If no argument provided, the config will be loaded from
+    # default location.
+    try:
+        client_api= __get_kubernetes_corev1client(
+                bearer_token=cluster_details["bearer_token"],
+                api_server_endpoint=cluster_details["api_server_endpoint"],
+            )
+        resp = client_api.delete_namespaced_pod(
+                name=k8s_object_name,
+                namespace="{}".format(namespace),
+                body=client.V1DeleteOptions(
+                    propagation_policy="Foreground", grace_period_seconds=5)
+            )
+
+        data=__format_data_for_create_pod(resp)
+        print("Pod deleted: {}".format(data))
+
+    except ApiException as e:
+        print("ERROR IN create_deployment:\n{}".format(e.body))
+        print("TYPE :{}".format(type(e)))
+        return __format_data_for_create_pod(e.body)
+
+
+if __name__ == '__main__':
+    cluster_details={
+        "bearer_token":"GKE-Bearer-Token",
+        "api_server_endpoint":"Ip-k8s-control-plane"
+    }
+    get_pods(cluster_details)
+    # create_pod(cluster_details)
+    # update_pod(cluster_details,k8s_object_name="nginx-pod-yamika")
+    # delete_pod(cluster_details,k8s_object_name="nginx-pod-yamika")
